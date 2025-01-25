a1a068277486260dbb495bd81d012d606e9aceb7
funnut <195337009+funnut@users.noreply.github.com>
2025-01-25 21:11:11 +0100

    teraz jest 5

---
+++ analizatortekstu/a001.py
@@ -0,0 +1,3 @@
+# Analizator tekstu
+# Napisz program, który analizuje tekst wprowadzony przez użytkownika, liczy słowa, znaki, zdania, a także wyszukuje najczęściej występujące słowo.
+
---
+++ bot/b001.py
@@ -0,0 +1,2 @@
+# Prosty bot konsolowy
+# Stwórz bota, który odpowiada na podstawowe pytania użytkownika (np. "Jak masz na imię?", "Ile masz lat?"). Może to być prosty chatbot.
