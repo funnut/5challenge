26182e79a649e7667f4a6cb68e29f0b25b9b139a
funnut <195337009+funnut@users.noreply.github.com>
2025-01-27 00:54:57 +0100

    z001 duzy postep

--- zgadnijgra/z001.py
+++ zgadnijgra/z001.py
@@ -15,13 +15,31 @@
 ### Generator losowych liczb
 ### Funkcja sprawdzajaca "odleglosc" inputu (wartosci bezwzgledne)
 
-losowa = 6
-strzal = input()
+losowa = 50
 
-def sprawdz_odl (losowa, strzal):
-	a = losowa
-	b = strzal
-	return a+b
 
-odleglosc = sprawdz_odl (losowa, strzal)
-print (odleglosc)
+def sprawdz_odleglosc (a, b):
+	return abs(a-b)
+
+def komentator (a):
+	if a <= 5:
+		return "Jestes juz bardzo blisko!"
+	elif a <= 10:
+		return "Blisko, blisko..."
+	elif a <= 20:
+		return "Bunkrow nie ma ale niedaleko"
+	elif a <= 50:
+		return "Kazdy miewa gorsze dni"
+	elif a <= 75:
+		return "Bardzo, bardzo zimno"
+
+
+while True:
+	strzal = int(input())
+	odleglosc = sprawdz_odleglosc(losowa, strzal)
+
+	if odleglosc:
+		print (komentator(odleglosc))
+	else:
+		print ("Strzal w dziesiatke!")
+		break
