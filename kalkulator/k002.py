89e963bd102be159d91df684864e5f8d0817ee0f
funnut <195337009+funnut@users.noreply.github.com>
2025-01-26 01:02:35 +0100

    k002 edycja

--- kalkulator/k002.py
+++ kalkulator/k002.py
@@ -1,12 +1,10 @@
 # 1. Kalkulator tekstowy
 # Napisz prosty kalkulator, który wykonuje podstawowe działania matematyczne, takie jak dodawanie, odejmowanie, mnożenie i dzielenie.
 
-############
 ### k002 ###
 ############
 
 ### Ta wersja ma wykonywac konkretne obliczenia na polecenie
-
 # pobierz x danych i zapisz
 # gdy sum, min, iloraz itd wykonaj obliczenia i zwroc wynik
 # first in first out
@@ -21,7 +19,34 @@
 #- iloraz
 ## break
 
+user_input = [3]
 
-liczby = ['3', '2', '5', 'end']
-print(liczby)
+while True:
+	print ('suma')
+	print ('roznica')
+	print ('iloczyn')
+	print ('iloraz')
+	print ('break')
+	print (user_input)
+	user_input = input("Podaj liczbe lub oblicz: ")
+	if user_input.lower() == 'suma':
+		user_input.append()
+
+
+
+
+
+
+
+
+
+
+#if user_input.lower() == 'suma': # .lower() ???
+
+
+
+
+
+#	liczby.append(input())
+
 
