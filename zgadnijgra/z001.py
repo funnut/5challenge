5923db2d8ea934a9810af8e741dc11e21d76f3af
funnut <195337009+funnut@users.noreply.github.com>
2025-01-26 22:45:52 +0100

    Pierwsze szkice

--- zgadnijgra/z001.py
+++ zgadnijgra/z001.py
@@ -1,2 +1,27 @@
 # Gra w zgadywanie liczby
-# Użytkownik zgaduje liczbę wylosowaną przez program w określonym zakresie (np. od 1 do 100). Program podpowiada, czy liczba jest za duża, za mała czy trafiona.
+# Użytkownik zgaduje liczbę wylosowaną przez program w określonym zakresie (i czasie?) (np. od 1 do 100). Program podpowiada, czy liczba jest za duża, za mała czy trafiona.
+
+# Zgadnij liczbe od 0 do 100!
+# Jestes bardzo blisko!
+# INPUT 69
+
+# Witaj!
+#   1. Rozpocznij gre
+#   2. Wyjdz z gry
+#
+
+# Podaj login
+
+### Generator losowych liczb
+### Funkcja sprawdzajaca "odleglosc" inputu (wartosci bezwzgledne)
+
+losowa = 6
+strzal = input()
+
+def sprawdz_odl (losowa, strzal):
+	a = losowa
+	b = strzal
+	return a+b
+
+odleglosc = sprawdz_odl (losowa, strzal)
+print (odleglosc)
