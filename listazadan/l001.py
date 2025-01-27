719f19ca86825f278c001cd78cf2b17c66c0f42e
funnut <195337009+funnut@users.noreply.github.com>
2025-01-27 16:33:47 +0100

    l001 ogolny zarys programu

--- listazadan/l001.py
+++ listazadan/l001.py
@@ -1,2 +1,38 @@
-# 3. Lista zadań (To-Do List)
+# Lista zadań (To-Do List)
 # Stwórz aplikację do zarządzania zadaniami, która pozwala użytkownikowi dodawać, usuwać i wyświetlać zadania.
+
+### l001 ###
+############
+
+# Witaj w l001!
+# add / del / show
+# >>
+
+user_input = input().strip()
+
+def sprawdz_input(user_input):
+	if user_input == 'add':
+		komenda = 'add'
+		return komenda
+	elif user_input == 'del':
+		komenda = 'del'
+		return komenda
+	elif user_input == 'show':
+		komenda = 'show'
+		return komenda
+	else:
+		komenda = 'eRRor'
+		return komenda
+
+
+def glowna_funkcja (a):  # moze (a, b) ?
+	if a == 'add':
+		print ("glowna_funkcja add")
+	elif a == 'del':
+		print ("glowna_funkcja del")
+	elif a == 'show':
+		print ("glowna_funkcja show")
+	elif a == 'eRRor':
+		print ("nieprawidlowe polecenie!")
+
+glowna_funkcja(sprawdz_input(user_input))
