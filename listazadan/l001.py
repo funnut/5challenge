9e7225a7129a014011db7c5c251f5f19bff716f8
funnut <195337009+funnut@users.noreply.github.com>
2025-02-05 02:51:29 +0100

    l001

--- listazadan/l001.py
+++ listazadan/l001.py
@@ -20,9 +20,8 @@ print (f"\n\n\nWitaj w lisku!\n{randrange(0,1000)} <- q <- h")
 def glowna_funkcja (polecenie):
 	if polecenie == 'add': # ADD
 		NOTKA = str(input("Wpisz notatke: ").strip())
-		print(NOTKA)
 		if len(NOTKA) != 0:
-			print ("_DONE_\n### notatka zapisana")
+			print ("works")
 			pobierz_input()
 		else:
 			pobierz_input()
@@ -40,6 +39,8 @@ def glowna_funkcja (polecenie):
 		pobierz_input()
 			###
 	elif polecenie in ['show', 's']: # SHOW
+		for line in read_data[-5:]:
+			print (line, end='')
 		pobierz_input()
 	elif polecenie[0] in ['show', 's'] and polecenie[1] == 'all':
 		print (read_data)
@@ -54,7 +55,7 @@ def glowna_funkcja (polecenie):
 	elif polecenie in ['help', 'h', 'menu', 'info', 'commands']:
 		print ("\n\nLisek is a free and OpenSource notes app for your terminal\nEnter q for quit\ncls for screen cleanup\nshow or s <- showing last 5 notes\nshow [t] <- showing t number of notes\nshow all <- listing all notes\ndel id035 <- deliting a specific note\ndel all <- deleting all notes")
 		pobierz_input()
-	elif polecenie in ['hey', 'hi', 'hello', 'welcome','yo']
+	elif polecenie in ['hey', 'hi', 'hello', 'welcome','yo']:
 		print ("\nNice having you here!\nShall we start?")
 		pobierz_input()
 	elif polecenie in ['end', 'exit', 'quit', 'q']:
