a874ea227ac76bcc1cad017799208e7ac7ebeead
funnut <195337009+funnut@users.noreply.github.com>
2025-02-07 09:17:10 +0100

    zmiana nazwy katalogu

---
+++ aword/a001.py
@@ -0,0 +1,6 @@
+# Analizator tekstu
+# Napisz program, który analizuje tekst wprowadzony przez użytkownika, liczy słowa, znaki, zdania, a także wyszukuje najczęściej występujące słowo.
+
+### a001 ###
+############
+
