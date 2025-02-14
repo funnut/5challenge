4252d8e71999e5a4c53116745c7ddc6675259c34
funnut <195337009+funnut@users.noreply.github.com>
2025-02-14 23:28:28 +0100

    a001 start

--- 4_awords/a001.py
+++ 4_awords/a001.py
@@ -4,3 +4,10 @@
 ### a001 ###
 ############
 
+# Witaj w programie do analizy tekstu.
+# Czy tekst jest w pliku .txt? (y/n)
+# >>
+
+
+sampletxt = ['To jest przekładowy tekst wprowadzony przez użytkownika. Jest on stworzony na potrzeby tworzenia programu do analizy tekstu. Tekst może być wprowadzony bezpośrednio do programu lub pobrany z pliku txt. Słowo "tekst" występuje tu 4 razy.']
+
